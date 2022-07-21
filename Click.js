f7726ae804d6a240cf9f45e78ac07f6dad67f882
ASavastru <smeag0leye>
2022-07-21 10:03:18 +0300

    Changed "onclick" alert solution to "eventListener"

--- Click.js
+++ Click.js
@@ -1,3 +1,9 @@
-function alertman(clicked) {
-    alert("You clicked " + clicked);
-}
+//function alertman(clicked) {
+//    alert("You clicked " + clicked);
+//}
+
+document.querySelectorAll(".inMonth").forEach(alertman => {
+    alertman.addEventListener("click", e => {
+        alert(e.target.dataset.date);
+    })
+});
